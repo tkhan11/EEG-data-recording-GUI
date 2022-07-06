1418d0ad146c86c395609bdfc761dabf3b4142a8
EEG-data-recording-GUI <khantanveer.1193@gmail.com>
2022-07-06 11:10:03 +0530

    updation

--- MotorTasks.py
+++ MotorTasks.py
@@ -1,5 +1,5 @@
 ﻿#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#coding: utf-8 
 ##
 ## AUTHOR: TANVEER AHMED KHAN
 ###
--- VEPTasks.py
+++ VEPTasks.py
@@ -1,6 +1,6 @@
 ﻿#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-##
+#coding: utf-8 
+#
 ## AUTHOR: TANVEER AHMED KHAN
 ###
 
